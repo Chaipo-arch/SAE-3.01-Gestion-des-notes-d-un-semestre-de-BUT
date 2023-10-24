6aa1b56bfacae483e4f641fba28a1d7de97e9d7a
ALEXANDRE BROUZES <alexandre.brouzes@iut.local>
2023-10-24 15:08:05 +0200

    premier commit

---
+++ noteManager/main/Evaluation.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package noteManager.main;
+
+/**
+ *
+ * @author alexandre.brouzes
+ */
+public class Evaluation {
+    /**  
+     * Attibut contenant un type de contrôle entre devoir sur table,
+       Qcm, tp noté
+     */
+    private String typeControle = "";
+    
+    /**
+     *  
+     */
+    private double coefficient = 0.0;
+    /**
+     *
+     */
+    private String date = "";
+    
+    
+}
