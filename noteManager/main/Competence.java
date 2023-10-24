07fb6a1ba4418ca63d6588d6ccdcf2ac40b014b4
ENZO CLUZEL <enzo.cluzel@iut.local>
2023-10-24 17:44:35 +0200

    stockage realisé

---
+++ noteManager/main/Competence.java
@@ -0,0 +1,22 @@
+package noteManager.main;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author enzo.cluzel
+ */
+public class Competence {
+
+    public String libelle;
+    
+    public Competence(String libelle) {
+        this.libelle = libelle;
+        
+    }
+}
