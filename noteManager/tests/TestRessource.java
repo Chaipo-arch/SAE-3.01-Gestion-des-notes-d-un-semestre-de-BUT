1b0531faa89e644cbed1c69e878b0ae5f92f070f
AHMED BRIBACH <ahmed.bribach@iut.local>
2023-10-27 16:42:25 +0200

    ajout de jeux de tests fin de l'heure de SAE je continuerais plus tard

--- noteManager/tests/TestRessource.java
+++ noteManager/tests/TestRessource.java
@@ -21,39 +21,28 @@ import static noteManager.tests.TestEvaluation.listeNoteValide;
 public class TestRessource {
     
     public static ArrayList<Ressource> ressourcesValide= new ArrayList<>() ;
+    public static ArrayList<Evaluation> listeDeToutEvaluation = new ArrayList<>();
     
-    public void jeuxDeData() throws Exception{
-        listeNoteValide.add(new Note(0.5));
-        listeNoteValide.add(new Note(10.524));
-        listeNoteValide.add(new Note(20));
-        listeNoteValide.add(new Note(12.256));
-        
-        listeEvaluationValide.add(new Evaluation("Maths",listeNoteValide.get(0)
-                ,"QCM",1,"25/01/2023"));
-        listeEvaluationValide.add(new Evaluation("Développement Web",listeNoteValide.get(1)
-                ,"Exam sur machine",100,"Mi septembre"));
-        listeEvaluationValide.add(new Evaluation("Sql",listeNoteValide.get(2)
-                ,"Ecrit",20,""));
-        listeEvaluationValide.add(new Evaluation("Anglais",listeNoteValide.get(3)
-                ,"Relevés TPs",50.5,"25/12/2024"));
-        
-        
-        listeEvaluationSansNote.add(new Evaluation("Cryptographie",null
-                ,"QCM",1,"25/01/2023"));
-        listeEvaluationSansNote.add(new Evaluation("Economie",null
-                ,"Exam sur machine",100,"Mi septembre"));
-        listeEvaluationSansNote.add(new Evaluation("Réseaux",null
-                ,"Ecrit",20,""));
-        listeEvaluationSansNote.add(new Evaluation("Programmation",null
-                ,"Relevés TPs",50.5,"25/12/2024"));
-        
-        
-        ressourcesValide.add(new Ressource("Programmation Général", 10, "id", "id"));
-        ressourcesValide.add(new Ressource("Mathématique", 20, "id", "id"));
-        ressourcesValide.get(0).ajouterEvaluation(listeEvaluationSansNote.get(0));
-        
-        
+    public boolean jeuxDeData() throws Exception{
+        boolean jeuValide = true;
+        try{
+            TestEvaluation.batterieDeTest();
+            listeDeToutEvaluation.addAll(listeEvaluationValide);
+            listeDeToutEvaluation.addAll(listeEvaluationSansNote);
+
+            ressourcesValide.add(new Ressource("Programmation Général", 10, "id", "id"));
+            ressourcesValide.add(new Ressource("Mathématique", 20, "id", "id"));
+            ressourcesValide.get(0).ajouterEvaluation(listeEvaluationSansNote.get(0));
+        }catch(Exception e){
+            jeuValide =  false;
+        }
+        try{
+            
+        }catch(Exception e){
+            
+        }
        
+        return jeuValide;
         
     }
 }
