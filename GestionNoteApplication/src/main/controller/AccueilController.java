dc8fc316a2c803fe78fd8b5b2c568d2439749488
ENZO CLUZEL <enzo.cluzel@iut.local>
2023-11-10 15:18:16 +0100

    Premier version de controlleur avec nouvelle idée

--- GestionNoteApplication/src/main/controller/AccueilController.java
+++ GestionNoteApplication/src/main/controller/AccueilController.java
@@ -3,12 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gestionnoteapp;
+package IHM;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +24,15 @@ import javafx.fxml.FXML;
  * @author robin.britelle
  */
 public class AccueilController implements Initializable {
+    
+
+    @FXML
+    private AnchorPane contenuPage;
+    
+    Stage fenetreSuivante = null;
+    
+    Parent fxml;
+
 
     /**
      * Initializes the controller class.
@@ -23,13 +40,29 @@ public class AccueilController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            fxml = FXMLLoader.load(getClass().getResource("accueil.fxml"));
+            contenuPage.getChildren().removeAll();
+            contenuPage.getChildren().setAll(fxml);
+        } catch (IOException ex) {
+            Logger.getLogger(AccueilController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    @FXML  
-    private void ClicChangementVueParametre() {
-        // échanger la vue courante avec celle des Parametres
-        GEstionNoteApp.activerParametres();
+     @FXML
+    void mesNotesActionBtn(ActionEvent event) {
+        try {
+            changerPage("page2.fxml");
+        } catch (IOException ex) {
+            
+        }
+    }
+
+    
+    public void changerPage(String page) throws IOException {
+        fxml = FXMLLoader.load(getClass().getResource("accueil.fxml"));
+        contenuPage.getChildren().removeAll();
+        contenuPage.getChildren().setAll(fxml);
     }
 
     
--- GestionNoteApplication/src/main/controller/GEstionNoteApp.java
+++ GestionNoteApplication/src/main/controller/GEstionNoteApp.java
@@ -1,134 +1,54 @@
 /*
- * Classe principale de l'application permettant d'afficher des fenêtres au contenu
- * différent : addition, soustraction ... (ce sont les vues qui changent)
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-package gestionnoteapp;
+package GestionNoteApplication.src.main.controller;
+
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
- * Cette classe est la classe principale de l'application permettant à l'utilisateur
- * d'effectuer plusieurs calculs via des vues différentes.
- * Le but est d'illustrer comment changer la scène associée associée à la fenêtre
- * principale.
  *
- * Au lancement de l'application, la vue principale contient 2 boutons : addition
- * et soustraction.
- * Selon le bouton cliqué, une scène différente prend la place de la vue principale :
- * soit celle de l'addition, soit celle de la soustraction.
- * Sur chacune de ces 2 vues, il y a un bouton retour permettant d'afficher à
- * nouveau la vue principale
- * @author R. Britelle
- * @version 1.0
- *
+ * @author enzo.cluzel
  */
 public class GEstionNoteApp extends Application {
     
-    /** Scène principale de l'application, celle qui contient les 2 boutons */
-    private static Scene sceneAccueil;
- 
-    /** Scène permettant de gérer l'addition */
-    private static Scene sceneParametres;
-    
-    private static Scene sceneAjouterEvaluation;
-    
-    private static Scene sceneMesEvaluations;
-    
-    private static Scene sceneMesMoyennes;
-    
-    private static Scene sceneImportation;
-    
-    private static Scene sceneCommuniquer;
-    
-
-    /** Fenêtre principale de l'application
-    * La scène qui lui est associée sera modifiée en fonction
-    * des clics de l'utilisateur
-    */
-    private static Stage fenetrePrincipale;
-    
+   
+    Stage fenetreSuivante = null;
+    Parent fxml;
     /**
-    * Permet de modifier la scène de la fenêtre principale
-    * pour qu'elle devienne celle de l'addition
-    */
-    public static void activerAcceuil() {
-        fenetrePrincipale.setScene(sceneAccueil);
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
- 
-    /**
-    * Permet de modifier la scène de la fenêtre principale
-    * pour qu'elle devienne celle de la soustraction
-    */
-    public static void activerParametres() {
-        fenetrePrincipale.setScene(sceneParametres);
-    }
-
 
     @Override
-    public void start(Stage primaryStage) {
-    try {
-    /*
-    * chargement de la vue de la scène principale dans le conteneur
-    * de type Parent
-    */
-    FXMLLoader chargeurFXML = new FXMLLoader();
-    chargeurFXML.setLocation(getClass().getResource("accueil.fxml"));
-    Parent conteneur = chargeurFXML.load();
+    public void start(Stage primaryStage) throws Exception {
+        try {
+                Parent fxml = FXMLLoader.load(getClass().getResource("accueil.fxml"));
+                Scene sceneActive = new Scene(fxml);
 
-    /*
-    * Création de la scène principale
-    */
-    sceneAccueil = new Scene(conteneur, 500, 300);
- 
-    /*
-    * Chargement de la vue de l'addition et
-    * création de la scène associée à cette vue
-    */
-    FXMLLoader chargeurFXMLAddition = new FXMLLoader();
-    chargeurFXMLAddition.setLocation(getClass().getResource("page2.fxml"));
-    conteneur = chargeurFXMLAddition.load();
-    sceneParametres = new Scene(conteneur, 500, 300);
- 
- 
- // on définit le titre, la hauteur et la largeur de la fenêtre principale
- primaryStage.setTitle("Application avec plusieurs fenêtres");
- primaryStage.setHeight(600);
- primaryStage.setWidth(1000);
- //primaryStage.minHeightProperty(100);
- //primaryStage.minWidthProperty(100);
- 
- primaryStage.setMinHeight(600);
- primaryStage.setMinWidth(1000);
- 
- 
- primaryStage.setMaxHeight(600);
- primaryStage.setMaxWidth(1000);
- /*
- * on associe la scène principale à la fenêtre principale
- * Cette dernière est stockée en tant qu'attribut afin d'être accessible
- * dans les méthodes activer... Celles qui permettent de rendre active
- * l'une des 3 scènes
- */
+               primaryStage.setScene(sceneActive);
+               primaryStage.show();
+               
 
- primaryStage.setScene(sceneAccueil);
- fenetrePrincipale = primaryStage;
- primaryStage.show();
-
- } catch (IOException e) {
-
- // TODO Auto-generated catch block
- e.printStackTrace();
- }
- }
-
- /**
- * Programme principal
- * @param args non utilisé
- */
- public static void main(String[] args) {
- launch(args);
- }
+        } catch (IOException ex) {
+                Logger.getLogger(GEstionNoteApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
 }
+    
