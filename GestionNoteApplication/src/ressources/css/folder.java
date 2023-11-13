d762c5b5076264501f10a5095913312b7a08bb15
ROBIN BRITELLE <robin.britelle@iut.local>
2023-11-13 10:50:09 +0100

    Rectifiaction IHM Parametre_1 / Ajout du css pour respecter le maquetage

--- GestionNoteApplication/src/ressources/css/folder.java
+++ GestionNoteApplication/src/ressources/css/folder.java
@@ -1,13 +1,33 @@
-public class VotreController {
+// Controller.java
 
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Controller {
     @FXML
-    private void handleSelectFolderButtonClick(ActionEvent event) {
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        File selectedDirectory = directoryChooser.showDialog(null);
+    private ComboBox<String> myComboBox;
 
-        if (selectedDirectory != null) {
-            System.out.println("Dossier sélectionné : " + selectedDirectory.getAbsolutePath());
-            // Vous pouvez faire quelque chose avec le dossier sélectionné
-        }
+    public void initialize() {
+        // Initialise la ComboBox avec des éléments
+        initializeComboBox();
+    }
+
+    private void initializeComboBox() {
+        // Crée une liste d'éléments
+        ObservableList<String> items = FXCollections.observableArrayList(
+                "Option 1",
+                "Option 2",
+                "Option 3"
+        );
+
+        // Ajoute les éléments à la ComboBox
+        myComboBox.setItems(items);
+
+        // Vous pouvez également ajouter des éléments individuellement
+        // myComboBox.getItems().addAll("Option 4", "Option 5");
     }
 }
