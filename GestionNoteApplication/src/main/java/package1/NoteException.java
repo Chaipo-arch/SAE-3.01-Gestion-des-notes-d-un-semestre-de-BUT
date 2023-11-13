dc25d0c5d7dc31fdeb9cc8d09ae76090ad6d4729
ROBIN BRITELLE <robin.britelle@iut.local>
2023-11-13 15:35:29 +0100

    modification arborecence et modification des class

---
+++ GestionNoteApplication/src/main/java/package1/NoteException.java
@@ -0,0 +1,19 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package noteManager.main.java;
+
+/**
+ *
+ * @author enzo.cluzel
+ */
+public class NoteException extends Exception {
+
+    public NoteException(String message) {
+    }
+    
+}
+
