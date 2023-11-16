8f5454eaee5ddc8743aba50c8f38a9e1e9d9d45c
ROBIN BRITELLE <robin.britelle@iut.local>
2023-11-16 16:09:22 +0100

    Modification et ajout de client/server pour le transfert et le stockage de Fichier CSV + TODO Verification du fichier transmis !

---
+++ GestionNoteApplication/src/main/java/packageCommunication/bordelCSV/Client.java
@@ -0,0 +1,50 @@
+package bordelCSV;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void sendCSVFileToServer(String serverIP, String filePath, int port) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.out.println("Le fichier spécifié n'existe pas.");
+                return;
+            }
+
+            Socket socket = new Socket(serverIP, port);
+
+            // Flux de sortie pour envoyer le fichier CSV au serveur
+            OutputStream out = socket.getOutputStream();
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println("Fichier CSV envoyé au serveur.");
+
+            // Fermeture des flux et du socket
+            fileInputStream.close();
+            out.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        String serverIP = "127.0.0.1"; // Adresse IP du serveur
+        String filePath = "Z:\\communication\\src\\Ressource\\test.csv"; // Chemin du fichier CSV à envoyer
+        int port = 8887; // Port du serveur
+
+        Client.sendCSVFileToServer(serverIP, filePath, port);
+        }
+        
+        
+        
+}
